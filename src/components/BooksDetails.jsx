8412a5332672e10a5b8e21f319f510ac4e4b7555
MarcHazime <marc.hazime@gmail.com>
2022-07-07 17:26:17 +0200

    add margin on same author list

--- src/components/BooksDetails.jsx
+++ src/components/BooksDetails.jsx
@@ -32,8 +32,7 @@ export default function BooksDetail() {
                             <p className="mb-2 text-xl  italic tracking-tight text-gray-900 dark:text-white">{book.author}</p>
                             <h4 className="text-xl text-blue-900 font-bold">isbn:</h4>
                             <p className="mb-2 text-xl  italic tracking-tight text-gray-900 dark:text-white">{book.isbn}</p>
-
-                            <h4 className="text-xl text-blue-900 font-bold">From the same author</h4>
+                            <h4 className="mb-3 text-xl text-blue-900 font-bold">From the same author</h4>
 
                             <ListGroup>
                                 {book.books.map((book) => (
@@ -50,7 +49,7 @@ export default function BooksDetail() {
                             <Link to={"/"} >
                                 <button
                                     type="button"
-                                    className=" mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+                                    className=" mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                                 >
                                     Back to list
                                 </button>
